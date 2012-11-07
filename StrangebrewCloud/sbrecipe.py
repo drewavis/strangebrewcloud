998e58f3b9be4d571febcbc39c5e7d375fda24c4
Drew Avis <drew.avis@gmail.com>
2012-11-07 16:57:25 -0500

    XML upload/dl enabled.

---
+++ StrangebrewCloud/sbrecipe.py
@@ -0,0 +1,37 @@
+import re
+from xml.dom import minidom
+from google.appengine.ext import db
+
+class SBRecipe(db.Model):
+    author = db.UserProperty()
+    comments = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
+    xml = db.TextProperty()
+    brewer = db.StringProperty()
+    name = db.StringProperty()
+    style = db.StringProperty()
+
+class RecipeData():
+    def __init__(self, xml):
+    
+        r = re.compile('<\?xml:stylesheet[^>]*>')
+        xml = r.sub('', xml)
+        xmldoc = minidom.parseString(xml.encode('latin1'))
+        self.og = xmldoc.getElementsByTagName('OG')[0].firstChild.data.strip()
+        self.fg = xmldoc.getElementsByTagName('FG')[0].firstChild.data.strip()
+        self.size = xmldoc.getElementsByTagName('SIZE')[0].firstChild.data.strip() 
+        self.size += " " + xmldoc.getElementsByTagName('SIZE_UNITS')[0].firstChild.data.strip()
+        malts = xmldoc.getElementsByTagName('FERMENTABLES')[0].getElementsByTagName('ITEM')        
+        self.malts = []
+        for item in malts :
+            self.malts.append(item.getElementsByTagName('AMOUNT')[0].firstChild.data.strip() + " " +
+                              item.getElementsByTagName('UNITS')[0].firstChild.data.strip() + " " +
+                              item.getElementsByTagName('MALT')[0].firstChild.data.strip())
+        
+        hops = xmldoc.getElementsByTagName('HOPS')[0].getElementsByTagName('ITEM')        
+        self.hops = []
+        for item in hops :
+            self.hops.append(item.getElementsByTagName('AMOUNT')[0].firstChild.data.strip() + " " +
+                              item.getElementsByTagName('UNITS')[0].firstChild.data.strip() + " " +
+                              item.getElementsByTagName('HOP')[0].firstChild.data.strip() + " (" +
+                              item.getElementsByTagName('TIME')[0].firstChild.data.strip() + " min)" )    
--- StrangebrewCloud/server.py
+++ StrangebrewCloud/server.py
@@ -1,33 +1,79 @@
 import webapp2
-import json
-from google.appengine.ext import db
+from xml.dom import minidom
+from sbrecipe import SBRecipe
 
-
-class SBRecipe(db.Model):
-    author = db.UserProperty()
-    comments = db.StringProperty(multiline=True)
-    date = db.DateTimeProperty(auto_now_add=True)
-    xml = db.TextProperty()
-    brewer = db.StringProperty()
-    name = db.StringProperty()
-    style = db.StringProperty()
-    
+   
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, webapp2 World!')
         
 class RecipeHandler(webapp2.RequestHandler):
-    def get(self,name=None):
-        if name is None or name == '':
-            name = 'no one'
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello , *' + name + '*!')
+    def get(self,name=None):        
+        # return all recipes w/ id's:
+        if name is None or name == '' or name == 'all':
+            resp='<xml>'
+            recipes_query = SBRecipe.all()
+            for recipe in recipes_query:
+                resp += '<recipe id=\''+str(recipe.key().id()) + '\' name=\'' + recipe.name \
+                    + '\' style=\'' + recipe.style + '\' />'
+            resp += '</xml>'  
+            
+            self.response.headers['Content-Type'] = 'application/xml'   
+            self.response.write(resp)
+            return     
+            
+        else:
+            # try to get ID
+            try:
+                r_id = int(name)
+                recipe = SBRecipe.get_by_id(r_id)
+                self.response.headers['Content-Type'] = 'application/xml'   
+                self.response.write(recipe.xml)
+                return 
+               
+               
+            except:
+                self.error(400)
+                self.response.out.write('Bad id.')      
+                return
+           
+       
+    def post(self, name=None):       
         
-    def post(self):
-        #Load the JSON values that were sent to the server
-        #dictionary = json.loads(self.request.body)
-        self.response.write('Hello , *' + self.request.body + '*!')
+        # first get the xml:
+        xmltxt = self.request.body
+        
+        # is it good?
+        try: 
+            xmldoc = minidom.parseString(xmltxt.encode('ISO-8859-1'))  
+        except:
+            self.error(400)
+            self.response.out.write('WTF? That is some bad xml, Poncho. \nPlease try again.')      
+            return
+    
+        if xmldoc.getElementsByTagName('STRANGEBREWRECIPE') == [] :
+            self.error(400)
+            self.response.out.write('That just doesn\'t look like a StrangeBrew Recipe to me.  Sorry.')      
+            return
+        
+        # create new recipe object
+        recipe = SBRecipe()
+        recipe.xml = xmltxt;
+        recipe.version = xmldoc.getElementsByTagName('STRANGEBREWRECIPE')[0].attributes['version'].value
+        recipe.brewer = xmldoc.getElementsByTagName('BREWER')[0].firstChild.data.strip()
+        recipe.name = xmldoc.getElementsByTagName('NAME')[0].firstChild.data.strip()
+        recipe.style = xmldoc.getElementsByTagName('STYLE')[0].firstChild.data.strip()
+        if xmldoc.getElementsByTagName('NOTES')[0].firstChild != None :
+            recipe.comments = xmldoc.getElementsByTagName('NOTES')[0].firstChild.data.strip()
+        
+        # does this already exist?
+        if SBRecipe.all().filter("name =", recipe.name).count(limit=10) > 0 :
+            self.error(400)
+            self.response.out.write('A recipe with that name already exists.')
+        else :
+            k = recipe.put()       
+            self.response.write('recipe id: ' + str(k.id()))
         
 
 app = webapp2.WSGIApplication([('/', MainPage),
